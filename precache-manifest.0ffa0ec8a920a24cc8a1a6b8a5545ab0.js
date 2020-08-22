916b2b98a13a1d2cbcf2474ffde0d791dd1e04ee
Gustavo Matheus Morinaga Cardoso <47375774+gmatthewsfeuer@users.noreply.github.com>
2020-08-22 15:02:21 -0300

    Updates

---
+++ precache-manifest.0ffa0ec8a920a24cc8a1a6b8a5545ab0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e24ec32a894036d48407fb3ff6d292ab",
+    "url": "/HotCode/index.html"
+  },
+  {
+    "revision": "6446094e53516daa1cac",
+    "url": "/HotCode/static/css/main.45c07e65.chunk.css"
+  },
+  {
+    "revision": "c4b6de26b3432ae5d91d",
+    "url": "/HotCode/static/js/2.306bcf30.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/HotCode/static/js/2.306bcf30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6446094e53516daa1cac",
+    "url": "/HotCode/static/js/main.a381faa9.chunk.js"
+  },
+  {
+    "revision": "e12bdd5e28b872c03613",
+    "url": "/HotCode/static/js/runtime-main.1ead08c5.js"
+  }
+]);
