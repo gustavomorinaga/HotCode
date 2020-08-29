79b8e3062837bf76571466c723ac339882f4e7a4
Gustavo Matheus Morinaga Cardoso <47375774+gmatthewsfeuer@users.noreply.github.com>
2020-08-29 03:39:50 -0300

    Updates

--- precache-manifest.b0965a3aa2415f9d97029fa2dcb2253a.js
+++ precache-manifest.b0965a3aa2415f9d97029fa2dcb2253a.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c19cde1401687eb444d6742a3aff8b17",
+    "revision": "d506100d7fb57d1bc2a8a667cbd11e0a",
     "url": "/HotCode/index.html"
   },
   {
-    "revision": "9617f6d441b6f83de311",
+    "revision": "6bb11784773f9d1a4eea",
     "url": "/HotCode/static/css/2.eb43dee1.chunk.css"
   },
   {
-    "revision": "6aae8b4b4b2d64cfdd4c",
-    "url": "/HotCode/static/css/main.e5d7e4ef.chunk.css"
+    "revision": "1a8eb5d6fd7459659acd",
+    "url": "/HotCode/static/css/main.74999a94.chunk.css"
   },
   {
-    "revision": "9617f6d441b6f83de311",
-    "url": "/HotCode/static/js/2.afd88b74.chunk.js"
+    "revision": "6bb11784773f9d1a4eea",
+    "url": "/HotCode/static/js/2.d18e46bc.chunk.js"
   },
   {
     "revision": "1937a0fc6634269e5f47f8181e27fdc7",
-    "url": "/HotCode/static/js/2.afd88b74.chunk.js.LICENSE.txt"
+    "url": "/HotCode/static/js/2.d18e46bc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6aae8b4b4b2d64cfdd4c",
-    "url": "/HotCode/static/js/main.3a077c59.chunk.js"
+    "revision": "1a8eb5d6fd7459659acd",
+    "url": "/HotCode/static/js/main.bb85f5c6.chunk.js"
   },
   {
     "revision": "e12bdd5e28b872c03613",
@@ -40,12 +40,24 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/HotCode/static/media/background-community.d6f806b7.svg"
   },
   {
+    "revision": "faabcfa225cd78ed050ad57e8ff685c3",
+    "url": "/HotCode/static/media/database.faabcfa2.png"
+  },
+  {
     "revision": "4bf4f3488e01306a4b44b68447e47b72",
     "url": "/HotCode/static/media/expo.4bf4f348.png"
   },
   {
+    "revision": "4410a7213b6d6041cc3922c683bea1a6",
+    "url": "/HotCode/static/media/gear.4410a721.png"
+  },
+  {
     "revision": "f1ae665d31373a9b81a5cae4f89b1209",
     "url": "/HotCode/static/media/logo.f1ae665d.svg"
+  },
+  {
+    "revision": "013663a91e315505b72835c8c5ec3bab",
+    "url": "/HotCode/static/media/monitor.013663a9.png"
   },
   {
     "revision": "ea8cc21145655376ad62be3988c5c9ef",
@@ -60,8 +72,20 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/HotCode/static/media/react.693d85f1.png"
   },
   {
+    "revision": "1cf679d9681c9c633d82b39f1005bbd4",
+    "url": "/HotCode/static/media/rocket.1cf679d9.png"
+  },
+  {
+    "revision": "b0fd3afd7684893ad34c836339d17f70",
+    "url": "/HotCode/static/media/smartphone.b0fd3afd.png"
+  },
+  {
     "revision": "954dfa3032fc963b3a63108b8e010723",
     "url": "/HotCode/static/media/square-versatiles.954dfa30.svg"
+  },
+  {
+    "revision": "a13208ca5823cc33de34b6808a1fa9e8",
+    "url": "/HotCode/static/media/varying-stripes.a13208ca.svg"
   },
   {
     "revision": "83f6cbb5b4ff9256f123f5d9b78c268b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/HotCode/precache-manifest.d0370904656072408771fa15f4cfa988.js"
+  "/HotCode/precache-manifest.b0965a3aa2415f9d97029fa2dcb2253a.js"
 );
 
 self.addEventListener('message', (event) => {
